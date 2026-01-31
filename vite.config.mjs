d41e85ebbcd6201a43f90710f0e12c9466113342
BALANARAYANA325 <rvbalanarayana@gmail.com>
2026-01-31 16:02:53 +0530

    Update base path to /11/ for new repo name

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: '/silent-signals/',
+  base: '/11/',
   plugins: [react()],
   resolve: {
     alias: {
